119f5cbb07c81c24c3fc7794e1330c34dcb63b81
nzAily <winsdesign.wd@gmail.com>
2024-12-27 01:23:44 +0700

    Fix namespace

--- src/block/WallCabinet.php
+++ src/block/WallCabinet.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace DavyCraft648\MCFurniture\block;
 
 use customiesdevs\customies\block\permutations\RotatableTrait;
+use customiesdevs\customies\block\permutations\Permutable;
+use pocketmine\block\Transparent;
 
-class WallCabinet extends \pocketmine\block\Transparent implements \customiesdevs\customies\block\permutations\Permutable{
-	use RotatableTrait;
+class WallCabinet extends Transparent implements Permutable {
+    use RotatableTrait;
 }
